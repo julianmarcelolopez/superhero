dd8ed4e31b85c63cba83cfcaa91a8b901bac7acf
Julian <julianmarcelolopez@gmail.com>
2022-01-22 09:54:13 -0300

    0.0.1 - initial commit spring initializr

---
+++ src/test/java/com/jlopez/w2m/superhero/SuperheroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlopez.w2m.superhero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
